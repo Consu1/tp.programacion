d4fc945e3256b276f62ca9ed0a8df68adbeae9d7
Consu1 <cperronellavar@udesa.edu.ar>
2020-11-25 23:19:25 -0300

    Cambios en el chart de peliculas

--- javascript/index.js
+++ javascript/index.js
@@ -1,8 +1,35 @@
 window.onload=function(){
 
     var apiKey= 'cc55526ed6d9221ada36a41066b7c9ea';
+    fetch(`https://api.themoviedb.org/3/tv/on_the_air?api_key=${apiKey}&language=en-US&page=1`)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (information) {
+            console.log(information);
 
-    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`)
+            var ul = document.querySelector (".pelis")
+
+
+            for (let i = 0; i < information.results.length; i++) {
+                var element = information.results[i];
+                ul.innerHTML  += `
+                
+                    <li>
+                    <a href="detail.html?tipo=serie&id=${element.id}">
+                    <img src="${linkimagen}${element.poster_path}" alt="">
+                    </a>
+                    </li>
+                ` 
+            }
+        })
+        .catch(function (error) {
+            console.log('El error fue: ' + error);
+        })
+
+    
+    
+    /*    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`)
 .then(function(response){
 return response.json();
 })
@@ -32,5 +59,5 @@ console.log(data);
 .catch(function(error){
 console.log('El error fue: ' + error);
 })
+*/
 }
-
