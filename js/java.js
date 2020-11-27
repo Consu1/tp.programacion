5aed993c9555148d23186b513c77b4e61d28697b
abriniell <69683991+abriniell@users.noreply.github.com>
2020-11-27 00:24:04 -0300

    arregle cosas

--- js/java.js
+++ js/java.js
@@ -34,7 +34,7 @@ window.addEventListener("load", function() {
         }
       })
   
-    /*fech de peliculas */
+    /*fetch de peliculas */
     fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=cc55526ed6d9221ada36a41066b7c9ea&language=en-US&page=1")
       .then(function(respuesta) {
         return respuesta.json();
@@ -45,7 +45,7 @@ window.addEventListener("load", function() {
         var pel = datos.results;
   
         for (var i = 0; i < pel.length; i++) {
-          sliderUl.innerHTML += '<li><a id="click_pelis" href="detalle.html?id=' + datos.results[i].id + '"> ' + '<img src="https://image.tmdb.org/t/p/w500/' + datos.results[i].poster_path + '">' + '</a></li>'
+          sliderUl.innerHTML += '<li><a id="click_pelis" href="detalles.html?id=' + datos.results[i].id + '"> ' + '<img src="https://image.tmdb.org/t/p/w500/' + datos.results[i].poster_path + '">' + '</a></li>'
         }
       })
     console.log("hola");
@@ -67,13 +67,6 @@ window.addEventListener("load", function() {
     buscarRandom.innerHTML = '<a href="info_serie.html?id=' + aleatorio() + '">' + 'Descubrir' + '</a>'
   
     //Se creo una funcion que permite hallar series random
-  
-  
-  
-  
-  
-  
-  
   
   
   })
